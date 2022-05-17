acc6115bdb7ff4931a94e489825a8fc65235e30d
A3ternus <avksaint1st@gmail.com>
2022-05-17 18:13:43 +0300

    test commit

---
+++ SCRIPTS/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const answers = [];
+answers[0] = prompt('Как ваше Имя?', '');
+answers[1] = prompt('Как ваше Фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+console.log(answers);
