27cd589ff729321da727927df7d366e159365755
A3ternus <avksaint1st@gmail.com>
2022-06-06 20:03:48 +0300

    22 less

--- SCRIPTS/script_different.js
+++ SCRIPTS/script_different.js
@@ -1,5 +1,107 @@
 'use strict'
 
+// УРОК 22 ПЕРЕДАЧА ПО ССЫЛКЕ, SPREAD
+
+// let a = 5,
+//     b = a;
+// b = b + 5;
+// console.log(b);
+
+// const obj = {  //ссылка
+//     a: 5,
+//     b: 1
+// };
+
+// const copy = obj;
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+// поверхностная копия объекта
+// function copy(mainObj) {
+//     let objCopy = {};
+
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+//     return objCopy;
+// }
+
+
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y: 4
+//     }
+// };
+
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 10;
+// newNumbers.c.x = 10; 
+
+// console.log(numbers);
+// console.log(newNumbers);
+
+// const add = {
+//     d: 17,
+//     e: 20
+// };
+
+// const clone = Object.assign({}, add); 
+// clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+
+// const oldArray = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
+// newArray[0] = 'g'
+
+// console.log(oldArray);
+// console.log(newArray);
+
+
+// const video = ['yt', 'vimeo', 'rt'],
+//       blogs = ['wordpress', 'lj', 'blogger'],
+//       internet = [...video, ...blogs, 'vk', 'fb'];
+
+// console.log(internet);
+
+// function log(a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// const num = [2, 3, 5];
+// log(...num);
+
+
+
+// const array = ['a', 'b'];
+
+// const newAarray = [...array];
+// newAarray[0] = 't';
+// console.log(array);
+// console.log(newAarray);
+
+
+// const q = {
+//     one: 1,
+//     two: 2
+// };
+
+// const newObj = {...q};
+// console.log(newObj);
+
 // УРОК 21 МАССИВЫ И ПСЕВДОМАССИВЫ
 
 // // const arr = [3, 21, 5];
